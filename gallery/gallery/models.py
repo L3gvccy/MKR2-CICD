88e2d3c5e08467c63da7537f44f2a93fee9fc3f8
L3gvccy <oleksandr_iv@knu.ua>
2025-05-23 13:02:11 +0300

    Add initial migration, models, views, and template for gallery app

--- gallery/gallery/models.py
+++ gallery/gallery/models.py
@@ -5,11 +5,11 @@ class Category(models.Model):
     name = models.CharField(max_length=50)
 
     def __str__(self):
-        return self.name
+        return self.name 
 
 
 class Image(models.Model):
-    title = models.CharField(max_length=100)
+    title = models.CharField(max_length=101)
     image = models.ImageField(upload_to='gallery_images/')
     categories = models.ManyToManyField(Category, blank=True)
     created_date = models.DateField()
--- gallery/gallery/views.py
+++ gallery/gallery/views.py
@@ -1,1 +1,13 @@
 from django.shortcuts import render, get_object_or_404
+from django.utils import timezone
+from datetime import timedelta
+from .models import Image, Category
+
+def gallery_view(request):
+    today = timezone.now().date()
+    one_month_ago = today - timedelta(days=30)
+    recent_images = Image.objects.filter(created_date__gte=one_month_ago)
+    return render(request, 'gallery.html', {'images': recent_images})
+
+def image_detail(request, pk):
+    pass
